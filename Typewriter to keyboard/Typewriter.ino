6c54f85d9fd120002742137d10c363620fd3bee0
BmnQuentin <59441455+BmnQuentin@users.noreply.github.com>
2020-04-18 17:40:49 +0200

    Update Typewriter.ino

--- Typewriter to keyboard/Typewriter.ino
+++ Typewriter to keyboard/Typewriter.ino
@@ -1,398 +1,128 @@
+#include <Keyboard.h>
+#include <HID.h>
+
 /******************************************************************************
-  Attention, think of adding a 100kOhm between Gnd and A0 to prevent floating value, around 350
+  Beware, think of adding a 100kOhm between Gnd and A0 to prevent floating value, around 350
 ******************************************************************************/
 
 const int SOFT_POT_PIN = A0; // Pin connected to softpot wiper
-const int etatNul = 10; // Defines state where no value has to be considered
-const int dureeFrappe = 100; // Minimal duration of a stroke
-int etatCourant;
-const int tolerance = 10;
-long derniereFrappe; // to prevent accidentl double letter
-const int pinEspace = 2;
-const int pinMaj = 3;
-boolean etatEspace;
-boolean etatMaj;
+const int stateNull = 1020; // Defines state where no value has to be considered
+int pinEspace = 2;
+int pinMaj = 4;
+int pinBack = 6;
+int pinEnter = 8;
+boolean stateEspace;
+boolean stateMaj;
+boolean stateBack;
+boolean stateEnter;
+const int delayKey = 50; //delay before taking value after  character is typed
+const int delayThreshold = 50; // delay in measuring loop when a key is pressed
+bool valueToPrint;
+bool specialValueToPrint;
+int currentKey;
+int currentSpecialKey;
+int keyToPrint;
+boolean spaceAllowed; // flag to prevent us from having multiple spaces
+boolean enterAllowed;
+boolean backAllowed;
 
 void setup()
 {
   Serial.begin(9600);
+  Keyboard.begin();
   pinMode(SOFT_POT_PIN, INPUT);
-  etatCourant = etatNul - 1;
-  derniereFrappe=millis();
-  pinMode(pinEspace,INPUT_PULLUP);
+  pinMode(pinEspace, INPUT_PULLUP);
   pinMode(pinMaj, INPUT_PULLUP);
+  pinMode(pinBack, INPUT_PULLUP);
+  pinMode(pinEnter, INPUT_PULLUP);
+  valueToPrint = 0;
+  spaceAllowed = 0;
+  enterAllowed = 0;
+  backAllowed = 0;
 }
 
 void loop()
 {
   // Lecture du softpot
-  int softPotADC = analogRead(SOFT_POT_PIN);
-  etatEspace = !digitalRead(pinEspace);
-  etatMaj=!digitalRead(pinMaj);
+  delay(delayKey);
+  int softPot = analogRead(SOFT_POT_PIN);
+  stateEspace = digitalRead(pinEspace);
+  stateBack = digitalRead(pinBack);
+  stateEnter = digitalRead(pinEnter);
+  //Serial.println(softPot);
 
-  if ((etatCourant - tolerance < softPotADC && softPotADC < etatCourant + tolerance) && millis()-derniereFrappe<dureeFrappe ) {
-    
-  }
-  else {
-    if(etatEspace) {
-      Serial.print(" ");
+  //We decide if space is maintained it enables us to have special values or special controls
+  while (!digitalRead(pinEspace)) {
+    delay(delayThreshold); //Necessary to avoid hysteresis
+    softPot = analogRead(SOFT_POT_PIN);
+    if (softPot <= stateNull) {
+      specialValueToPrint = 1;
+      if (softPot < currentSpecialKey ) {
+        currentSpecialKey = softPot;
+      }
     }
-    if (softPotADC > etatNul) {
-      Serial.println(softPotADC);
-      etatCourant = softPotADC;
-      if (etatMaj) {
-        alphabetMax(softPotADC);
-      }
-      else {
-      alphabetMin(softPotADC);
-      }
-      derniereFrappe=millis();
+    else {
+      spaceAllowed = 1;
     }
   }
-  delay(dureeFrappe);
-}
 
-void alphabetMin(int entree)
-{
-  if (entree < 25)
-  {
-    Serial.print("a");
+  if (specialValueToPrint) {
+    //Serial.println(currentKey);
+    keyToPrint = currentSpecialKey;
+    currentSpecialKey = stateNull + 1;
+    specialValueToPrint = 0;
+    spaceAllowed = 0;
+    alphabetSpecial(keyToPrint);
   }
-  else if (entree<40)
-  {
-    Serial.print("q");
+
+  if (!stateEspace && spaceAllowed) {
+    sendtext(" ");
+    spaceAllowed = 0;
   }
-    else if (entree<70)
-  {
-    Serial.print("é");
+
+  //Back
+  if (stateBack) {
+    backAllowed = 1;
   }
-      else if (entree<90)
-  {
-    Serial.print("w");
+
+  if (!stateBack && backAllowed) {
+    backspacekey(1);
+    backAllowed = 0;
   }
-    else if (entree<110)
-  {
-    Serial.print("z");
+
+  //Enter
+  if (stateEnter) {
+    enterAllowed = 1;
   }
-    else if (entree<140)
-  {
-    Serial.print("s");
+
+  if (!stateEnter && enterAllowed) {
+    sendkeypad("E");
+    enterAllowed = 0;
   }
-    else if (entree<160)
-  {
-    Serial.print("\"");
+
+  // All maj and min characters
+  while (softPot <= stateNull ) {
+    //Serial.println(softPot);
+    valueToPrint = 1;
+    stateMaj = !digitalRead(pinMaj);
+    softPot = analogRead(SOFT_POT_PIN);
+    //Serial.println(currentKey);
+    if (softPot < currentKey ) {
+      currentKey = softPot;
+    }
+    delay(delayThreshold);
   }
-    else if (entree<180)
-  {
-    Serial.print("x");
-  }
-    else if (entree<200)
-  {
-    Serial.print("e");
-  }
-    else if (entree<220)
-  {
-    Serial.print("d");
-  }
-    else if (entree<240)
-  {
-    Serial.print("'");
-  }
-    else if (entree<260)
-  {
-    Serial.print("c");
-  }
-    else if (entree<290)
-  {
-    Serial.print("r");
-  }
-    else if (entree<310)
-  {
-    Serial.print("f");
-  }
-    else if (entree<330)
-  {
-    Serial.print("(");
-  }
-    else if (entree<360)
-  {
-    Serial.print("v");
-  }
-    else if (entree<380)
-  {
-    Serial.print("t");
-  }
-    else if (entree<400)
-  {
-    Serial.print("g");
-  }
-    else if (entree<430)
-  {
-    Serial.print("-");
-  }
-    else if (entree<450)
-  {
-    Serial.print("b");
-  }
-    else if (entree<480)
-  {
-    Serial.print("y");
-  }
-    else if (entree<500)
-  {
-    Serial.print("h");
-  }
-    else if (entree<530)
-  {
-    Serial.print("è");
-  }
-    else if (entree<560)
-  {
-    Serial.print("n");
-  }
-    else if (entree<580)
-  {
-    Serial.print("u");
-  }
-    else if (entree<600)
-  {
-    Serial.print("j");
-  }
-    else if (entree<630)
-  {
-    Serial.print("_");
-  }
-    else if (entree<650)
-  {
-    Serial.print(",");
-  }
-      else if (entree<680)
-  {
-    Serial.print("i");
-  }
-      else if (entree<710)
-  {
-    Serial.print("k");
-  }
-    else if (entree<730)
-  {
-    Serial.print("ç");
-  }
-    else if (entree<760)
-  {
-    Serial.print(";");
-  }
-    else if (entree<780)
-  {
-    Serial.print("o");
-  }
-    else if (entree<800) 
-  {
-    Serial.print("l");
-  }
-    else if (entree<830)
-  {
-    Serial.print("à");
-  }
-    else if (entree<860)
-  {
-    Serial.print(":");
-  }
-    else if (entree<880)
-  {
-    Serial.print("p");
-  }
-    else if (entree<910)
-  {
-    Serial.print("m"); 
-  }
-    else if (entree<940)
-  {
-    Serial.print(")"); 
-  }
-    else if (entree<970)
-  {
-    Serial.print("="); 
-  }
-    else if (entree<990)
-  {
-    Serial.print("^"); 
-  }
-    else if (entree<1020)
-  {
-    Serial.print("ù"); 
+
+  if (valueToPrint) {
+    //Serial.println(currentKey);
+    keyToPrint = currentKey;
+    currentKey = stateNull + 1;
+    valueToPrint = 0;
+    if (stateMaj) {
+      alphabetMax(keyToPrint);
+    }
+    else {
+      alphabetMin(keyToPrint);
+    }
   }
 }
-
-void alphabetMax(int entree)
-{
-  if (entree < 25)
-  {
-    Serial.print("A");
-  }
-  else if (entree<40)
-  {
-    Serial.print("Q");
-  }
-    else if (entree<70)
-  {
-    Serial.print("2");
-  }
-      else if (entree<90)
-  {
-    Serial.print("W");
-  }
-    else if (entree<110)
-  {
-    Serial.print("Z");
-  }
-    else if (entree<140)
-  {
-    Serial.print("S");
-  }
-    else if (entree<160)
-  {
-    Serial.print("3");
-  }
-    else if (entree<180)
-  {
-    Serial.print("X");
-  }
-    else if (entree<200)
-  {
-    Serial.print("E");
-  }
-    else if (entree<220)
-  {
-    Serial.print("D");
-  }
-    else if (entree<240)
-  {
-    Serial.print("4");
-  }
-    else if (entree<260)
-  {
-    Serial.print("C");
-  }
-    else if (entree<290)
-  {
-    Serial.print("R");
-  }
-    else if (entree<310)
-  {
-    Serial.print("F");
-  }
-    else if (entree<330)
-  {
-    Serial.print("5");
-  }
-    else if (entree<360)
-  {
-    Serial.print("V");
-  }
-    else if (entree<380)
-  {
-    Serial.print("T");
-  }
-    else if (entree<400)
-  {
-    Serial.print("G");
-  }
-    else if (entree<430)
-  {
-    Serial.print("6");
-  }
-    else if (entree<450)
-  {
-    Serial.print("B");
-  }
-    else if (entree<480)
-  {
-    Serial.print("Y");
-  }
-    else if (entree<500)
-  {
-    Serial.print("H");
-  }
-    else if (entree<530)
-  {
-    Serial.print("7");
-  }
-    else if (entree<560)
-  {
-    Serial.print("N");
-  }
-    else if (entree<580)
-  {
-    Serial.print("U");
-  }
-    else if (entree<600)
-  {
-    Serial.print("J");
-  }
-    else if (entree<630)
-  {
-    Serial.print("8");
-  }
-    else if (entree<650)
-  {
-    Serial.print("?");
-  }
-      else if (entree<680)
-  {
-    Serial.print("I");
-  }
-      else if (entree<710)
-  {
-    Serial.print("K");
-  }
-    else if (entree<730)
-  {
-    Serial.print("9");
-  }
-    else if (entree<760)
-  {
-    Serial.print(".");
-  }
-    else if (entree<780)
-  {
-    Serial.print("O");
-  }
-    else if (entree<800) 
-  {
-    Serial.print("L");
-  }
-    else if (entree<830) // we turn the & in a 1
-  {
-    Serial.print("1");
-  }
-    else if (entree<860)
-  {
-    Serial.print("/");
-  }
-    else if (entree<880)
-  {
-    Serial.print("P");
-  }
-    else if (entree<910)
-  {
-    Serial.print("M"); 
-  }
-    else if (entree<940) // we turn the ° in 0
-  {
-    Serial.print("0"); 
-  }
-    else if (entree<970)
-  {
-    Serial.print("+"); 
-  }
-    else if (entree<990)
-  {
-    Serial.print("!"); // we turn the ¨ in !
-  }
-    else if (entree<1020)
-  {
-    Serial.print("%");
-  }
-}
-
