fbea6d9eb5dbdd39b4cb8b7b521c20a58bda4458
BmnQuentin <59441455+BmnQuentin@users.noreply.github.com>
2020-04-18 17:42:09 +0200

    Create Typewriter_alphabet.ino

---
+++ Typewriter to keyboard/Typewriter_alphabet.ino
@@ -0,0 +1,515 @@
+void alphabetMin(int entree)
+{
+  if (entree < 30)
+  {
+    sendtext("a"); 
+  }
+  else if (entree < 50)
+  {
+    sendtext("q"); 
+  }
+  else if (entree < 70)
+  {
+    sendtext("é");
+  }
+  else if (entree < 90)
+  {
+    sendtext("w"); 
+  }
+  else if (entree < 115)
+  {
+    sendtext("z"); 
+  }
+  else if (entree < 140)
+  {
+    sendtext("s");
+  }
+  else if (entree < 170)
+  {
+    sendtext("\"");
+  }
+  else if (entree < 200)
+  {
+    sendtext("x");
+  }
+  else if (entree < 220)
+  {
+    sendtext("e");
+  }
+  else if (entree < 240)
+  {
+    sendtext("d");
+  }
+  else if (entree < 260)
+  {
+    sendtext("'");
+  }
+  else if (entree < 280)
+  {
+    sendtext("c");
+  }
+  else if (entree < 310)
+  {
+    sendtext("r");
+  }
+  else if (entree < 340)
+  {
+    sendtext("f");
+  }
+  else if (entree < 360)
+  {
+    sendtext("(");
+  }
+  else if (entree < 380)
+  {
+    sendtext("v");
+  }
+  else if (entree < 410)
+  {
+    sendtext("t");
+  }
+  else if (entree < 430)
+  {
+    sendtext("g");
+  }
+  else if (entree < 460)
+  {
+    sendtext("-");
+  }
+  else if (entree < 480)
+  {
+    sendtext("b");
+  }
+  else if (entree < 510)
+  {
+    sendtext("y");
+  }
+  else if (entree < 530)
+  {
+    sendtext("h");
+  }
+  else if (entree < 560)
+  {
+    sendtext("è");
+  }
+  else if (entree < 580)
+  {
+    sendtext("n");
+  }
+  else if (entree < 600)
+  {
+    sendtext("u");
+  }
+  else if (entree < 630)
+  {
+    sendtext("j");
+  }
+  else if (entree < 650)
+  {
+    sendtext("_");
+  }
+  else if (entree < 680)
+  {
+    sendtext(",");
+  }
+  else if (entree < 700)
+  {
+    sendtext("i");
+  }
+  else if (entree < 730)
+  {
+    sendtext("k");
+  }
+  else if (entree < 750)
+  {
+    sendtext("ç");
+  }
+  else if (entree < 770)
+  {
+    sendtext(";");
+  }
+  else if (entree < 800)
+  {
+    sendtext("o");
+  }
+  else if (entree < 820)
+  {
+    sendtext("l");
+  }
+  else if (entree < 850)
+  {
+    sendtext("à");
+  }
+  else if (entree < 870)
+  {
+    sendtext(":");
+  }
+  else if (entree < 900)
+  {
+    sendtext("p");
+  }
+  else if (entree < 930)
+  {
+    sendtext("m");
+  }
+  else if (entree < 950)
+  {
+    sendtext(")");
+  }
+  else if (entree < 980)
+  {
+    sendtext("=");
+  }
+  else if (entree < 1000)
+  {
+    sendtext("^");
+  }
+  else if (entree < stateNull)
+  {
+    sendtext("ù");
+  }
+}
+
+void alphabetMax(int entree)
+{
+  if (entree < 30)
+  {
+    sendtext("A");
+  }
+  else if (entree < 50)
+  {
+    sendtext("Q");
+  }
+  else if (entree < 70)
+  {
+    sendtext("2");
+  }
+  else if (entree < 90)
+  {
+    sendtext("W");
+  }
+  else if (entree < 115)
+  {
+    sendtext("Z");
+  }
+  else if (entree < 140)
+  {
+    sendtext("S");
+  }
+  else if (entree < 170)
+  {
+    sendtext("3");
+  }
+  else if (entree < 200)
+  {
+    sendtext("X");
+  }
+  else if (entree < 220)
+  {
+    sendtext("E");
+  }
+  else if (entree < 240)
+  {
+    sendtext("D");
+  }
+  else if (entree < 260)
+  {
+    sendtext("4");
+  }
+  else if (entree < 280)
+  {
+    sendtext("C");
+  }
+  else if (entree < 310)
+  {
+    sendtext("R");
+  }
+  else if (entree < 340)
+  {
+    sendtext("F");
+  }
+  else if (entree < 360)
+  {
+    sendtext("5");
+  }
+  else if (entree < 380)
+  {
+    sendtext("V");
+  }
+  else if (entree < 410)
+  {
+    sendtext("T");
+  }
+  else if (entree < 430)
+  {
+    sendtext("G");
+  }
+  else if (entree < 460)
+  {
+    sendtext("6");
+  }
+  else if (entree < 480)
+  {
+    sendtext("B");
+  }
+  else if (entree < 510)
+  {
+    sendtext("Y");
+  }
+  else if (entree < 530)
+  {
+    sendtext("H");
+  }
+  else if (entree < 560)
+  {
+    sendtext("7");
+  }
+  else if (entree < 580)
+  {
+    sendtext("N");
+  }
+  else if (entree < 600)
+  {
+    sendtext("U");
+  }
+  else if (entree < 630)
+  {
+    sendtext("J");
+  }
+  else if (entree < 650)
+  {
+    sendtext("8");
+  }
+  else if (entree < 680)
+  {
+    sendtext("?");
+  }
+  else if (entree < 700)
+  {
+    sendtext("I");
+  }
+  else if (entree < 730)
+  {
+    sendtext("K");
+  }
+  else if (entree < 750)
+  {
+    sendtext("9");
+  }
+  else if (entree < 770)
+  {
+    sendtext(".");
+  }
+  else if (entree < 800)
+  {
+    sendtext("O");
+  }
+  else if (entree < 820)
+  {
+    sendtext("L");
+  }
+  else if (entree < 850)
+  {
+    sendtext("1");
+  }
+  else if (entree < 870)
+  {
+    sendtext("/");
+  }
+  else if (entree < 900)
+  {
+    sendtext("P");
+  }
+  else if (entree < 930)
+  {
+    sendtext("M");
+  }
+  else if (entree < 950)
+  {
+    sendtext("0");
+  }
+  else if (entree < 980)
+  {
+    sendtext("+");
+  }
+  else if (entree < 1000)
+  {
+    sendtext("¨");
+  }
+  else if (entree < stateNull)
+  {
+    sendtext("%");
+  }
+}
+
+void alphabetSpecial(int entree)
+{
+  if (entree < 30) //^A
+  {
+    selectall();
+  }
+  else if (entree < 50)
+  {
+    sendtext("Q");
+  }
+  else if (entree < 70) //key 2
+  {
+    sendtext("€");
+  }
+  else if (entree < 90)
+  {
+    sendtext("W");
+  }
+  else if (entree < 115)
+  {
+    sendkeycombi(KEY_LEFT_CTRL, 'z'); //^Z
+  }
+  else if (entree < 140)
+  {
+    sendkeycombi(KEY_LEFT_CTRL, 's'); //^S
+  }
+  else if (entree < 170) //key 3
+  {
+    sendtext("#");
+  }
+  else if (entree < 200) //^x
+  {
+    cut();
+  }
+  else if (entree < 220)
+  {
+    sendtext("E");
+  }
+  else if (entree < 240)
+  {
+    sendtext("D");
+  }
+  else if (entree < 260) //key 4
+  {
+    sendtext("{");
+  }
+  else if (entree < 280) //^C
+  {
+    copy();
+  }
+  else if (entree < 310)
+  {
+    sendkeycombi(KEY_LEFT_CTRL, 'r'); //^R
+  }
+  else if (entree < 340)
+  {
+    sendkeycombi(KEY_LEFT_CTRL, 'f'); //^F
+  }
+  else if (entree < 360) //key 5
+  {
+    sendtext("[");
+  }
+  else if (entree < 380) //paste
+  {
+    paste();
+  }
+  else if (entree < 410)
+  {
+    sendtext("T");
+  }
+  else if (entree < 430)
+  {
+    sendtext("G");
+  }
+  else if (entree < 460) //key 6
+  {
+    sendtext("!");
+  }
+  else if (entree < 480)
+  {
+    sendtext("B");
+  }
+  else if (entree < 510)
+  {
+    sendtext("Y");
+  }
+  else if (entree < 530)
+  {
+    sendtext("H");
+  }
+  else if (entree < 560) //key 7
+  {
+    sendtext("<");
+  }
+  else if (entree < 580)
+  {
+    sendtext("N");
+  }
+  else if (entree < 600)
+  {
+    sendtext("U");
+  }
+  else if (entree < 630)
+  {
+    sendtext("J");
+  }
+  else if (entree < 650) //key 8
+  {
+    sendtext(">");
+  }
+  else if (entree < 680)
+  {
+    sendtext("?");
+  }
+  else if (entree < 700) // key i
+  {
+    sendtext("!");
+  }
+  else if (entree < 730)
+  {
+    sendtext("K");
+  }
+  else if (entree < 750) //9 key -> @
+  {
+    sendtext("@");
+  }
+  else if (entree < 770)
+  {
+    sendtext(".");
+  }
+  else if (entree < 800)
+  {
+    sendtext("O");
+  }
+  else if (entree < 820)
+  {
+    sendtext("L");
+  }
+  else if (entree < 850) //key à
+  {
+    sendtext("}");
+  }
+  else if (entree < 870) //key :
+  {
+    sendtext(":");
+  }
+  else if (entree < 900)
+  {
+    sendtext("P");
+  }
+  else if (entree < 930)
+  {
+    sendtext("M");
+  }
+  else if (entree < 950) //key °
+  {
+    sendtext("]");
+  }
+  else if (entree < 980)
+  {
+    sendtext("+");
+  }
+  else if (entree < 1000)
+  {
+    sendtext("¨");
+  }
+  else if (entree < stateNull)
+  {
+    sendtext("%");
+  }
+}
